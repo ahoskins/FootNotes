fe6e393e82ee7bb0e864df10fd06fafb1637dcd6
Andrew Hoskins <andrew.hoskins16@gmail.com>
2016-02-13 20:48:04 -0700

    remove contacts and token auth (unused anyway)

--- src/background/background.js
+++ src/background/background.js
@@ -13,17 +13,11 @@ function showExtension() {
 	executeScript()
 	.then(getProfileUserInfo)
 	.then(function(info) { // keep info in the closure
-		getToken()
-		.then(getContacts)
-		.then(function(contacts) { // keep contacts in the closure
-			let processedContacts = processContacts(contacts);
-			queryTabs()
-			.then(function(tabs) {
-				chrome.tabs.sendMessage(tabs[0].id, {
-					'userName': info.email.slice(0, info.email.indexOf('@')),
-					'contacts': processedContacts
-				});
-			})
+		queryTabs()
+		.then(function(tabs) {
+			chrome.tabs.sendMessage(tabs[0].id, {
+				'userName': info.email.slice(0, info.email.indexOf('@'))
+			});
 		})
 	})
 }
@@ -40,41 +34,8 @@ function queryTabs(info) {
 	})
 }
 
-function getToken() {
-  return new Promise(function(resolve, reject) {
-    chrome.identity.getAuthToken({'interactive': true}, resolve);
-  });
-}
-
-function getContacts(token) {
-  return new Promise(function(resolve, reject) {
-    let xhr = new XMLHttpRequest();
-    xhr.open('GET', 'https://www.google.com/m8/feeds/contacts/default/full', true);
-    xhr.setRequestHeader('Authorization', 'Bearer ' + token);
-		xhr.setRequestHeader('GData-Version', 3.0);
-    xhr.setRequestHeader('Content-Type', 'application/json');
-    xhr.send();
-    xhr.onload = resolve;
-  });
-}
-
 function getProfileUserInfo() {
 	return new Promise(function(resolve, reject) {
 		chrome.identity.getProfileUserInfo(resolve)
 	})
-}
-
-function processContacts(raw) {
-  let children = (new DOMParser()).parseFromString(raw.currentTarget.response,"text/xml").documentElement.children;
-  let emails = [];
-  for (let i = 0; i < children.length; i++) {
-    if (children[i].nodeName !== 'entry') continue;
-    let grandchildren = children[i].children;
-    for (let j = 0; j < grandchildren.length; j++) {
-      if (grandchildren[j].nodeName !== 'gd:email') continue;
-      emails.push(grandchildren[j].attributes.getNamedItem('address').nodeValue);
-    }
-  }
-
-  return emails;
 }
